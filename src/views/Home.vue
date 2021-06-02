43b92981aad13d5675ace165ad8143c6c8be0295
Tanzim <tanzim67@gmail.com>
2021-06-02 18:35:00 +0600

    Directory changed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
   <div class="flex h-screen bg-gray-900 antialiased">
   <div class="relative m-auto">
     <div class="z-50 p-3 mt-10 max-w-xs w-xs grid grid-cols-1 cursor-default select-none rounded bg-gray-400 dark:bg-gray-900 shadow-lg">
-      <div class="font-semibold text-xl mb-2 ml-2 text-gray-900 dark:text-white">Project Name</div>
+      <div class="font-semibold text-xl mb-2 ml-2 text-gray-900 dark:text-white">Project Name New</div>
       <div class="flex">
         <button
           class="transform mouse-pointer bg-gray-900 text-sm text-white mx-2 py-1 px-2 font-semibold rounded hover:bg-gray-700 dark:hover:bg-gray-600 hover:scale-90   focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Live</button>
